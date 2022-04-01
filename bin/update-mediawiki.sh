7341b532c825f9c01c03a8b6c8ac60485dde6c9c
Nick Ray <nray@nray.io>
2022-04-01 10:54:12 -0600

    Update update script

--- bin/update-mediawiki.sh
+++ bin/update-mediawiki.sh
@@ -5,12 +5,20 @@ git config --global user.name "Your Name"
 
 # Update core, extensions, and skins.
 time for i in . extensions/*/ skins/*/; do
-  echo "************Updating $i************"
-  git -C "$i" fetch --depth=1
-  git -C "$i" reset --hard origin
-  composer install --working-dir="$i" --no-dev 
+  git -C "$i" fetch --depth=1 &
+done
+
+wait
+time for i in . extensions/*/ skins/*/; do
+  git -C "$i" reset --hard origin &
+done
+
+wait
+time for i in . extensions/*/ skins/*/; do
+  composer install --working-dir="$i" --no-dev &
 done
 
 
+wait
 # Update the database schema.
 php maintenance/update.php
