6aa0963417a7566335f43cdf5eb105fbe1078224
Nick Ray <nray@nray.io>
2022-04-03 22:26:20 -0600

    Add cli to pixel.js

--- pixel.js
+++ pixel.js
@@ -26,25 +26,69 @@ function createSpawn( command, args ) {
 	} );
 }
 
-async function init() {
+/**
+ * @param {'test'|'reference'} type
+ * @param {any} opts
+ */
+async function start( type, opts ) {
 	try {
-		const args = process.argv.slice( 2 );
 		// Start docker containers.
 		await createSpawn( 'docker-compose', [ 'up', '-d' ] );
 		// Execute main.js.
 		await createSpawn(
 			'docker-compose',
-			[ 'exec', 'mediawiki', '/scripts/main.js', ...args ]
+			[ 'exec', 'mediawiki', '/scripts/main.js', JSON.stringify( opts ) ]
 		);
 		// Execute Visual regression
 		await createSpawn(
 			'docker-compose',
-			[ 'run', 'visual-regression', args.includes( 'reference' ) ? 'reference' : 'test', '--config', 'backstop.config.js' ]
+			[ 'run', 'visual-regression', type, '--config', 'backstop.config.js' ]
 		);
 
 	} catch ( err ) {
 		console.log( `Exited with code ${err}` );
 	}
+
 }
 
-init();
+function setupCli() {
+	const { program } = require( 'commander' );
+	const branchOpt = /** @type {const} */ ( [
+		'-b, --branch <name-of-branch>',
+		'Name of branch. Can be `master` or a release branch (e.g. `origin/wmf/1.37.0-wmf.19`)',
+		'master'
+	] );
+	const changeIdOpt = /** @type {const} */ ( [
+		'-c, --change-id <Change-Id...>',
+		'The Change-Id to use'
+	] );
+
+	program
+		.name( 'pixel.js' )
+		.description( 'Welcome to the pixel CLI to perform visual regression testing' );
+
+	program
+		.command( 'reference' )
+		.description( 'Create reference (baseline) screenshots that your test snapshots will be compared against.' )
+		.requiredOption( ...branchOpt )
+		.option( ...changeIdOpt )
+		.action( ( opts ) => {
+			console.log( 'in reference', opts );
+			start( 'reference', opts );
+		} );
+
+	program
+		.command( 'test' )
+		.description( 'Create test screenshots and compare them against the reference screenshots.' )
+		.requiredOption( ...branchOpt )
+		.option( ...changeIdOpt )
+		.action( ( opts ) => {
+			console.log( 'in test', opts );
+			start( 'test', opts );
+		} );
+
+	program.parse();
+
+}
+
+setupCli();
--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,6 @@
 #!/usr/bin/env node
 const checkoutPatch = require( './checkout-patch' );
 const repos = require( '../repositories.json' );
-const { program } = require( 'commander' );
-const branchOpt = /** @type {const} */ ( [
-	'-b, --branch <name-of-branch>',
-	'Name of branch. Can be `master` or a release branch (e.g. `origin/wmf/1.37.0-wmf.19`)',
-	'master'
-] );
-const changeIdOpt = /** @type {const} */ ( [
-	'-c, --change-id <Change-Id...>',
-	'The Change-Id to use'
-] );
+const opts = JSON.parse( process.argv[ 2 ] );
 
-program
-	.name( 'pixel.js' )
-	.description( 'Welcome to the pixel CLI to perform visual regression testing' );
-
-program
-	.command( 'reference' )
-	.description( 'Create reference (baseline) screenshots that your test snapshots will be compared against.' )
-	.requiredOption( ...branchOpt )
-	.option( ...changeIdOpt )
-	.action( ( opts ) => {
-		console.log( 'in reference', opts );
-		checkoutPatch( opts.changeId ?? [], repos );
-	} );
-
-program
-	.command( 'test' )
-	.description( 'Create test screenshots and compare them against the reference screenshots.' )
-	.requiredOption( ...branchOpt )
-	.option( ...changeIdOpt )
-	.action( ( opts ) => {
-		console.log( 'in test', opts );
-		checkoutPatch( opts.changeId ?? [], repos );
-	} );
-
-program.parse();
+checkoutPatch( opts.changeId ?? [], repos );
