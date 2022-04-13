c46a23279cc261c7aca9e2dfce199ec8661044a2
Jon Robson <jdlrobson@gmail.com>
2022-04-13 15:13:15 -0700

    Add await statements

--- src/engine-scripts/puppet/sidebarState.js
+++ src/engine-scripts/puppet/sidebarState.js
@@ -1,5 +1,5 @@
 const menuState = require( './menuState' );
 module.exports = async (page, hashtags) => {
     const isClosed = hashtags.includes( '#sidebar-closed' );
-    menuState(page, '#mw-sidebar-button', isClosed);
+    await menuState(page, '#mw-sidebar-button', isClosed);
 };
--- src/engine-scripts/puppet/userMenuState.js
+++ src/engine-scripts/puppet/userMenuState.js
@@ -1,5 +1,5 @@
 const menuState = require( './menuState' );
 module.exports = async (page, hashtags) => {
     const isClosed = hashtags.includes( '#userMenu-closed' );
-    menuState(page, '#p-personal-checkbox', isClosed);
+    await menuState(page, '#p-personal-checkbox', isClosed);
 };
