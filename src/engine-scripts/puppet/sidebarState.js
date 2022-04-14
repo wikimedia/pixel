f57d5ec29705947d68ccc4c8bf3338d6bb8e3785
Jon Robson <jdlrobson@gmail.com>
2022-04-13 17:18:21 -0700

    Don't run when no hash tags provided

--- src/engine-scripts/puppet/sidebarState.js
+++ src/engine-scripts/puppet/sidebarState.js
@@ -1,5 +1,10 @@
 const menuState = require( './menuState' );
 module.exports = async (page, hashtags) => {
+    const isOpen = hashtags.includes( '#sidebar-open' );
     const isClosed = hashtags.includes( '#sidebar-closed' );
+    if ( !isOpen && !isClosed ) {
+        // not applicable.
+        return;
+    }
     await menuState(page, '#mw-sidebar-button', isClosed);
 };
--- src/engine-scripts/puppet/userMenuState.js
+++ src/engine-scripts/puppet/userMenuState.js
@@ -1,5 +1,10 @@
 const menuState = require( './menuState' );
 module.exports = async (page, hashtags) => {
+    const isOpen = hashtags.includes( '#userMenu-open' );
     const isClosed = hashtags.includes( '#userMenu-closed' );
+    if ( !isOpen && !isClosed ) {
+        // not applicable.
+        return;
+    }
     await menuState(page, '#p-personal-checkbox', isClosed);
 };
