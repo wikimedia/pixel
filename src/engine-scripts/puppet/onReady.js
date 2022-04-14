840992162f22d3e75ce43e8a8b4bc1d91567343f
Jon Robson <jdlrobson@gmail.com>
2022-04-14 08:57:40 -0700

    Add Vector 2022 hashtag

--- src/engine-scripts/puppet/onReady.js
+++ src/engine-scripts/puppet/onReady.js
@@ -2,9 +2,13 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   const label = scenario.label;
   const hashtagsMatch = label.match( /(#[^ ]*)/g);
-  const hashTags = hashtagsMatch ? hashtagsMatch[0] : [];
+  const hashtags = hashtagsMatch ? hashtagsMatch[0] : [];
   await require('./clickAndHoverHelper')(page, scenario);
-  await require('./sidebarState')(page, hashTags);
-  await require('./userMenuState')(page, hashTags);
+
+  // These only apply to Vector 2022
+  if ( hashtags.includes( '#vector-2022' ) ) {
+    await require('./sidebarState')(page, hashtags);
+    await require('./userMenuState')(page, hashtags);
+  }
   // add more ready handlers here...
 };
--- src/engine-scripts/puppet/userMenuState.js
+++ src/engine-scripts/puppet/userMenuState.js
@@ -1,10 +1,5 @@
 const menuState = require( './menuState' );
 module.exports = async (page, hashtags) => {
-    const isOpen = hashtags.includes( '#userMenu-open' );
     const isClosed = hashtags.includes( '#userMenu-closed' );
-    if ( !isOpen && !isClosed ) {
-        // not applicable.
-        return;
-    }
     await menuState(page, '#p-personal-checkbox', isClosed);
 };
