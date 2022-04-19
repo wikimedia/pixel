55ca79738a144301657c49cfb4296cc6e83edfa6
Jon Robson <jdlrobson@gmail.com>
2022-04-19 16:09:27 -0700

    Disable animations for UI regression runs
    See T306229
    This will fix the dancing tabs issue.

---
+++ src/engine-scripts/puppet/jsReady.js
@@ -0,0 +1,24 @@
+module.exports = async (page) => {
+	await page.emulateMediaFeatures([
+		{ name: 'prefers-reduced-motion', value: 'reduce' },
+	]);
+	await page.evaluate( () => {
+		window.dispatchEvent( new Event( 'resize' ) );
+		const skin = mw.config.get( 'skin' );
+		let moduleName;
+		switch( skin ) {
+			case 'vector-2022':
+				moduleName = 'skins.vector.js';
+				break;
+			case 'vector':
+				moduleName = 'skins.vector.legacy.js';
+				break;
+			default:
+				return true;
+		}
+		return mw.loader.getState( moduleName ) === 'ready';
+	});
+
+	// wait for animation frame or two.
+	await page.waitForTimeout(2000);
+};
