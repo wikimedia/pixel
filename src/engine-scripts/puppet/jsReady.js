3378f7b960fda877a28ffcac6263e6cdbef75471
Nicholas Ray <nray@wikimedia.org>
2022-04-21 12:38:32 -0600

    Revise rAF statement

--- src/engine-scripts/puppet/jsReady.js
+++ src/engine-scripts/puppet/jsReady.js
@@ -2,7 +2,7 @@ module.exports = async (page) => {
 	await page.emulateMediaFeatures([
 		{ name: 'prefers-reduced-motion', value: 'reduce' },
 	]);
-	await page.evaluate( () => {
+	await page.evaluate( async () => {
 		const skin = mw.config.get( 'skin' );
 		let moduleName;
 		switch( skin ) {
@@ -15,17 +15,19 @@ module.exports = async (page) => {
 			default:
 				return true;
 		}
-		return new Promise((resolve) => {
-			setInterval(() => {
+
+		// Poll the state of the module until it is ready.
+		await new Promise((resolve) => {
+			const id = setInterval(() => {
 				if ( mw.loader.getState( moduleName ) === 'ready' ) {
+					clearInterval( id );
 					resolve(true);
 				}
 			}, 500 );
 		});
-	});
 
-	// Wait until the next frame before resolving.
-	await page.evaluate( () => {
+		// Wait until the next frame before resolving. collapsibleTabs.js in Vector
+		// and Vector-2022 make use of rAF.
 		return new Promise( resolve => {
 			requestAnimationFrame( () => {
 				requestAnimationFrame( () => {
@@ -33,5 +35,6 @@ module.exports = async (page) => {
 				} );
 			} );
 		} );
-	} );
+
+	});
 };
