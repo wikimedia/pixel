82a01ffd44f848a04e7a892e2d5c738502cd3166
Nick Ray <nray@nray.io>
2022-04-25 13:45:07 -0600

    Add eslint rule for wait for timeout (#11)
    * Add fastForwardAnimation.js and eslint rule for waitForTimeout
    
    * Using `waitForTimeout` can significantly slow down the tests so add eslint rule that guards against its usage.
    
    * Fix existing usage of `waitForTimeout` by bringing any currently running animations to the their end state. Please note that `prefers-reduced-motion` could also be used here when the work from T306229 is complete.

---
+++ src/engine-scripts/puppet/fastForwardAnimations.js
@@ -0,0 +1,28 @@
+/**
+ * Fast forward the document's running animation(s) so that we don't have to
+ * wait around for them to finish.
+ *
+ * Please note this will only work for animations that use css transitions or
+ * the web animation api. It will NOT work for animations that manipulate the
+ * DOM (e.g. altering the style attribute).
+ *
+ * @param {import("puppeteer").Page} page
+ */
+async function fastForwardAnimations( page ) {
+	return page.evaluate( () => {
+		// Adapted from https://github.com/microsoft/playwright/blob/0a401b2d86a39df85e57ad30bcec9ef81618abd0/packages/playwright-core/src/server/screenshotter.ts#L174
+		document.getAnimations().forEach( ( animation ) => {
+			if ( animation.playbackRate === 0 || !animation.effect ) {
+				return;
+			}
+
+			if ( Number.isFinite( animation.effect.getComputedTiming().endTime ) ) {
+				animation.finish();
+			} else {
+				animation.cancel();
+			}
+		} );
+	} );
+}
+
+module.exports = fastForwardAnimations;
