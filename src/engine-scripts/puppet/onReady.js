47253d4ad9870110532b95fed3ad9b86f1a9e185
Jon Robson <jdlrobson@gmail.com>
2022-06-27 15:40:43 -0700

    Update onReady.js

--- src/engine-scripts/puppet/onReady.js
+++ src/engine-scripts/puppet/onReady.js
@@ -37,7 +37,7 @@ module.exports = async ( page, scenario ) => {
 	}
 
 	if ( hashtags.includes( '#toc-open' ) ) {
-		await require( './menuState' )( page, '#vector-toc-collapsed-button', false )
+		await require( './menuState' )( page, '#vector-toc-collapsed-button', false );
 	}
 	// add more ready handlers here...
 };
