b5edbdebdd063a00b9428679d93a3f51b5af98d1
Jon Robson <jdlrobson@gmail.com>
2022-09-01 13:52:22 -0700

    Table of contents: Wait after a scroll for bolding
    Follow up to 314617b252
    Fixes: #96

--- src/engine-scripts/puppet/scroll.js
+++ src/engine-scripts/puppet/scroll.js
@@ -3,6 +3,12 @@ module.exports = async ( page ) => {
 		window.scrollBy( 0, window.innerHeight );
 		return true;
 	} );
-	// wait for bolding of active section
-	await page.waitForSelector( 'sidebar-toc-list-item-active' );
+	// wait for bolding of active section or toc collapsed button
+	await page.waitForSelector( 'sidebar-toc-list-item-active, #vector-toc-collapsed-button' );
+
+	// Wait for the scroll to settle - as it has side effects on things like the
+	// table of contents bolding of sections.
+	// Forgive us Nick.
+	// eslint-disable-next-line no-restricted-properties
+	await page.waitForTimeout( 2500 );
 };
