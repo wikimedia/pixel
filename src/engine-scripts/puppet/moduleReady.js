324ba1d73fda6deb388cd9332ab0ca174b742d11
Jon Robson <jdlrobson@gmail.com>
2022-09-09 15:32:26 -0700

    Search: Avoid module loading error
    - Make more assertions to avoid module loading error
    - Search: Use viewport width to decide which element to target

--- src/engine-scripts/puppet/moduleReady.js
+++ src/engine-scripts/puppet/moduleReady.js
@@ -9,7 +9,9 @@ module.exports = async ( page, moduleName ) => {
 					resolve();
 				}
 				if ( times > 10 ) {
-					reject( `Cannot find module ${m}. Is scenario setup with correct hashtags?` );
+					// eslint-disable-next-line no-undef
+					const debug = `codex=${mw.loader.getState( '@wikimedia/codex' )}vector=${mw.loader.getState( 'skins.vector.search' )}`;
+					reject( `Cannot find module ${m} (${debug}). Is scenario setup with correct hashtags?` );
 				}
 				times++;
 			}, 1000 );
--- src/engine-scripts/puppet/search.js
+++ src/engine-scripts/puppet/search.js
@@ -13,22 +13,28 @@ module.exports = async ( page, hashtags ) => {
 		await page.setOfflineMode( true );
 	}
 
+	const viewportWidth = page.viewport().width;
 	const isStickyHeaderScenario = hashtags.includes( '#search-sticky' );
 	const selectorSearchToggle = isStickyHeaderScenario ?
-		'.vector-sticky-header-search-toggle' : '.search-toggle';
+		'.vector-sticky-header-search-toggle' :
+		// If not sticky header scenario either the search toggle or the search input
+		// must be visible before continuing the test.
+		( viewportWidth < 1000 ) ? '.search-toggle' : '#p-search input';
 	const selectorSearchInput = isStickyHeaderScenario ?
 		'#vector-sticky-header input[name="search"]' : 'input[name="search"]';
 	const selectorSearchSuggestion = isStickyHeaderScenario ?
 		'#vector-sticky-header .vector-search-box-vue li a' : '#searchform li a';
 
+	// Check the sticky header has loaded if necessary.
+	if ( isStickyHeaderScenario ) {
+		await page.waitForSelector( '.vector-sticky-header-visible' );
+	}
+
 	// Click toggle if necessary to reveal input
-	const button = await page.waitForSelector( selectorSearchToggle );
-	await button.boxModel().then( async ( box ) => {
-		// If bounding box is null then the button is hidden on the page.
-		if ( box !== null ) {
-			await button.click();
-		}
+	const button = await page.waitForSelector( selectorSearchToggle, {
+		visible: true
 	} );
+	await button.click();
 	// Focus the server side rendered search to trigger the loading of Vue.
 	await page.focus( selectorSearchInput );
 	if ( isOffline ) {
@@ -40,6 +46,8 @@ module.exports = async ( page, hashtags ) => {
 		} );
 		await fastForwardAnimations( page );
 	} else {
+		// Make sure Codex kicked in.
+		await page.waitForSelector( '.cdx-typeahead-search' );
 		// Wait for Vue to load.
 		await moduleReady( page, 'vue' );
 		// focus and type into the newly added input
