7aa75449909eb0b7b7b79ef427c642119b19e56f
Jon Robson <jdlrobson@gmail.com>
2022-09-09 17:13:00 -0700

    Disable bolding in Pixel tests

--- src/engine-scripts/puppet/scroll.js
+++ src/engine-scripts/puppet/scroll.js
@@ -4,7 +4,9 @@ module.exports = async ( page ) => {
 		return true;
 	} );
 	// wait for bolding of active section or toc collapsed button
-	await page.waitForSelector( 'sidebar-toc-list-item-active, #vector-toc-collapsed-button' );
+	await page.waitForSelector( '.sidebar-toc-list-item-active, #vector-toc-collapsed-button', {
+		visible: true
+	} );
 
 	// Wait for the scroll to settle - as it has side effects on things like the
 	// table of contents bolding of sections.
