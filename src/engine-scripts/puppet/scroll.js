760dd7e9951240ae3d691d977d88cf2ee04be4ba
Nicholas Ray <nray@wikimedia.org>
2022-10-07 17:00:29 -0600

    Fix Protocol error (Runtime.callFunctionOn): Target closed.
    During the web-maintained test group, this error would surface on every
    `#scroll` test case. A closer inspection of the onReady.js revealed that we
    weren't actually waiting on the scroll promise to resolve which is what was
    causing the error.
    
    * Also, remove the setTimeout and waitForSelector calls as it doesn't appear
    anything was waiting on those.
    
    Bug: T317665

--- src/engine-scripts/puppet/scroll.js
+++ src/engine-scripts/puppet/scroll.js
@@ -3,14 +3,4 @@ module.exports = async ( page ) => {
 		window.scrollBy( 0, window.innerHeight );
 		return true;
 	} );
-	// wait for bolding of active section or toc collapsed button
-	await page.waitForSelector( '.sidebar-toc-list-item-active, #vector-toc-collapsed-button', {
-		visible: true
-	} );
-
-	// Wait for the scroll to settle - as it has side effects on things like the
-	// table of contents bolding of sections.
-	// Forgive us Nick.
-	// eslint-disable-next-line no-restricted-properties
-	await page.waitForTimeout( 2500 );
 };
