d66ca0b5ed58f6bd2c28ba0bc96d165a6cf8e855
Jon Robson <jdlrobson@gmail.com>
2023-07-28 16:09:28 -0700

    Wait for VisualEditor as well as wikitext editor

--- src/engine-scripts/puppet/onReady.js
+++ src/engine-scripts/puppet/onReady.js
@@ -41,7 +41,7 @@ module.exports = async ( page, scenario ) => {
 		// Run click handlers if necessary.
 		if ( hashtags.includes( '#click-edit' ) ) {
 			await clickBtn( page, '#ca-edit' );
-			await page.waitForSelector( '.editor-container' );
+			await page.waitForSelector( '.editor-container, .ve-ui-surface' );
 		}
 		if ( hashtags.includes( '#click-language' ) ) {
 			await clickBtn( page, '#language-selector a' );
