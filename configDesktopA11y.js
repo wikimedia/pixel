e9a4a84929dcde470fb5eac13ff93eaee2b19c2f
bwang <bwang@wikimedia.org>
2024-02-13 11:30:02 -0600

    Upgrade pa11y to 7.0.0

--- configDesktopA11y.js
+++ configDesktopA11y.js
@@ -16,13 +16,11 @@ const testDefaults = {
 	includeWarnings: true,
 	includeNotices: true,
 	ignore: [
-		// Prevent axe-core from flagging all TOC links as incorrect color contrast
-		'color-contrast',
 		// Prevent contrast ratio error on absolutely positioned elements
 		'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs'
 	],
 	chromeLaunchConfig: {
-		headless: true,
+		headless: 'new',
 		args: [
 			'--no-sandbox',
 			'--disable-setuid-sandbox'
@@ -36,12 +34,12 @@ module.exports = {
 	tests: [
 		{
 			name: 'default',
-			url: BASE_URL + '/wiki/Test?useskin=vector2022',
+			url: BASE_URL + '/wiki/Test?useskin=vector-2022',
 			...testDefaults
 		},
 		{
 			name: 'logged_in',
-			url: BASE_URL + '/wiki/Test?useskin=vector2022',
+			url: BASE_URL + '/wiki/Test?useskin=vector-2022',
 			...testDefaults
 		}
 	]
