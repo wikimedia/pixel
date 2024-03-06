f269832929aab21f07b3bfa0215fc37431e7354b
bwang <bwang@wikimedia.org>
2024-03-06 13:08:27 -0600

    Disable color-contrast rule for now

--- configMobileA11y.js
+++ configMobileA11y.js
@@ -17,7 +17,8 @@ const testDefaults = {
 	includeNotices: true,
 	ignore: [
 		// Prevent contrast ratio error on absolutely positioned elements
-		'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs'
+		'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs',
+		'color-contrast'
 	],
 	chromeLaunchConfig: {
 		headless: true,
