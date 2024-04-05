4099e78c0a0a5acb8b1c390cd309256d7fc86db6
montehurd <mhurd@wikimedia.org>
2024-04-05 15:32:44 -0700

    Move GROUP_CONFIG and A11Y_GROUP_CONFIG to config dir

--- pixel.js
+++ pixel.js
@@ -8,6 +8,8 @@ const simpleSpawn = new SimpleSpawn();
 const fs = require( 'fs' );
 const CONTEXT_PATH = `${__dirname}/context.json`;
 const makeReport = require( './src/makeReportIndex.js' );
+const GROUP_CONFIG = require( './config/groupConfig' );
+const A11Y_GROUP_CONFIG = require( './config/a11yGroupConfig' );
 
 /*
  * @param {string[]} opts
@@ -100,76 +102,6 @@ ${markerString}`
 		console.error( e );
 	}
 }
-
-/**
- * Each group has an assigned priority based on how regular they need to run.
- * For suites with lots of test where code seldom changes priority 2 and 3 are
- * preferred. Feel free to modify these as development priorities shift.
- * Priority 1 - run every hour
- * Priority 2 - run every 12 hours
- * Priority 3 - run every 24 hours
- */
-const GROUP_CONFIG = {
-	login: {
-		name: 'Login and sign up pages',
-		priority: 3,
-		config: 'configLogin.js'
-	},
-	'web-maintained': {
-		name: 'Extensions and skins maintained by web team',
-		priority: 3,
-		config: 'configWebMaintained.js'
-	},
-	echo: {
-		name: 'Echo badges',
-		priority: 3,
-		config: 'configEcho.js'
-	},
-	'desktop-dev': {
-		name: 'Zebra Vector 2022 skin',
-		priority: 3,
-		config: 'configDesktopDev.js'
-	},
-	desktop: {
-		name: 'Vector 2022 skin',
-		priority: 1,
-		config: 'configDesktop.js'
-	},
-	mobile: {
-		name: 'Minerva and MobileFrontend',
-		priority: 1,
-		config: 'configMobile.js'
-	},
-	'campaign-events': {
-		priority: 2,
-		config: 'configCampaignEvents.js'
-	},
-	codex: {
-		priority: 1,
-		config: 'configCodex.js'
-	},
-	wikilambda: {
-		priority: 2,
-		config: 'configWikiLambda.js'
-	}
-};
-
-const A11Y_GROUP_CONFIG = {
-	desktop: {
-		name: 'Vector 2022 accessibility',
-		priority: 2,
-		a11y: true,
-		logResults: true,
-		config: 'configDesktopA11y.js'
-	},
-	mobile: {
-		name: 'Minerva and MobileFrontend accessibility',
-		priority: 2,
-		a11y: true,
-		logResults: true,
-		config: 'configMobileA11y.js'
-	}
-};
 
 /**
  * @param {string} groupName
