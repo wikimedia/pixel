3b0a25d851dcd512f2dfb61b934fc0de81e26042
montehurd <mhurd@wikimedia.org>
2024-06-26 21:43:59 -0700

    Redirect clean & rebuild scripts stderr to stdout

--- rebuild.sh
+++ rebuild.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Stop and remove Docker containers, networks, and volumes
-./clean.sh
+./clean.sh 2>&1
 
 # Build the pixel-base-regression image
-./build-base-regression-image.sh
+./build-base-regression-image.sh 2>&1
 
 # Build or rebuild services
-docker compose --progress=plain --project-directory . -f ./docker-compose.yml build
+docker compose --progress=plain --project-directory . -f ./docker-compose.yml build 2>&1
 
 # Remove any dangling images
-docker image prune -f
+docker image prune -f 2>&1
