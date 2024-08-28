8512b6f7882a991224eb87880504ffb15ab63ac6
montehurd <mhurd@wikimedia.org>
2024-08-27 19:00:59 -0700

    Stop the novnc container when the stop script is called

--- stop.sh
+++ stop.sh
@@ -2,3 +2,6 @@
 
 # Stop all Docker containers associated with Pixel
 docker compose --progress=plain --project-directory . -f ./docker-compose.yml stop
+
+# Started this one detached so have to bring it down separately
+docker compose stop novnc || true
