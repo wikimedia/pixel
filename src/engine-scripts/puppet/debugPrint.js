ac45105879ac6da935831b5467a9518416a445b3
montehurd <mhurd@wikimedia.org>
2024-08-28 19:05:13 -0700

    Update the scenario url logged to be loadable from the host

--- src/engine-scripts/puppet/debugPrint.js
+++ src/engine-scripts/puppet/debugPrint.js
@@ -6,7 +6,7 @@
 const printScenario = async (scenario) => {
 	console.log('\nSCENARIO >');
 	console.log('  ' + scenario.label);
-	console.log('  ' + scenario.url);
+	console.log('  ' + scenario.url.replace('http://mediawiki-web', 'http://localhost').replace('8080', process.env.PIXEL_MW_HOST_PORT));
 	if (process.env.SCENARIO_DETAILS === '1') {
 		console.log(JSON.stringify(scenario, null, 2));
 	}
