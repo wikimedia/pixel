ff7bef23f93ce164127244ea12f9a69152630e3b
Jon Robson <jdlrobson@gmail.com>
2024-09-04 17:18:58 -0700

    Update limited width puppet script to work with appearance menu

--- src/engine-scripts/puppet/limitedWidth.js
+++ src/engine-scripts/puppet/limitedWidth.js
@@ -6,16 +6,6 @@ const clickBtn = require( './clickBtn' );
  * @param {string[]} hashtags
  */
 module.exports = async ( page, hashtags ) => {
-	// limit width by default to ensure consistency between tests
-	// Only disable when hashtag is provided
-	const isAppearanceEnabled = await page.evaluate( () => {
-		return document.documentElement.classList.contains( 'vector-feature-appearance-enabled' );
-	} );
-
-	if ( isAppearanceEnabled ) {
-		// FIXME: Handle limited width with client pref menu instead
-		return;
-	}
 
 	const isDisabled = hashtags.includes( '#limited-width-disabled' );
 	const isExcluded = await page.evaluate( () => {
@@ -36,19 +26,10 @@ module.exports = async ( page, hashtags ) => {
 	} );
 
 	if ( ( isDisabled && isCurrentlyEnabled ) || ( !isDisabled && !isCurrentlyEnabled ) ) {
-		const limitedWidthButtonSelector = '.vector-limited-width-toggle';
-		const selectorAfterToggle = `.vector-feature-limited-width-clientpref-${isCurrentlyEnabled ? '0' : '1'}`;
+		const suffix = isCurrentlyEnabled ? '0' : '1';
+		const limitedWidthButtonSelector = `#skin-client-pref-vector-feature-limited-width-value-${suffix}`;
+		const selectorAfterToggle = `.vector-feature-limited-width-clientpref-${suffix}`;
 		await page.waitForSelector( limitedWidthButtonSelector );
 		await clickBtn( page, limitedWidthButtonSelector, selectorAfterToggle );
-
-		// If we have disabled limited width a popup will show.
-		// Make sure it gets dismissed before showing screenshot.
-		if ( isCurrentlyEnabled ) {
-			await page.waitForSelector( '.vector-popup-notification' );
-			// Click the body to dismiss the notification.
-			await page.evaluate( async () => {
-				document.body.click();
-			} );
-		}
 	}
 };
